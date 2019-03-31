3791a406bc1bd636a5aa5c6d0dca0d474c40405a
Wang Lingdong <wld22145@126.com>
2019-03-31 20:46:36 +0800

    add k-fold validation

--- Project1/data_process.py
+++ Project1/data_process.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import os
 from config import path_dir
+from sklearn.model_selection import KFold
 
 path_feature = path_dir + r"\AwA2-features.txt"
 path_filename = path_dir + r"\AwA2-filenames.txt"
@@ -17,6 +18,8 @@ def parse_labels():
     labels = file_labels.readlines()
     for i in range(data_size):
         labels[i] = int(labels[i][:-1])
+    labels = np.array(labels)
+    print("labels shape:",labels.shape)
     return labels
 
 def parse_filenames():
@@ -52,11 +55,36 @@ def show_information(index):
     print("filename:",filenames[index])
     print("feature:",features[index])
 
+# prepare data
 filenames = parse_filenames()
 labels = parse_labels()
 features = parse_features()
 
 show_information(0)
+
+# TODO: dimension reduction
+# here I simply choose the first 50 features as example
+reduced_features = features[:,:50]
+print("reduced features shape:",reduced_features.shape)
+
+# k-fold validation
+kfold= KFold(n_splits=5,random_state =None)
+fold_cnt = 0
+for train_index,test_index in kfold.split(reduced_features,labels):
+    train_features = reduced_features[train_index]
+    train_labels = labels[train_index]
+    test_features = reduced_features[test_index]
+    test_labels = labels[test_index]
+    fold_cnt += 1
+    print("fold:", fold_cnt)
+    print("train features shape:",train_features.shape)
+    print("train labels shape:",train_labels.shape)
+    print("test features shape:",test_features.shape)
+    print("test labels shape:", test_labels.shape)
+    # TODO: machine learning algorithm
+    # based on train/test features/labels, you can conduct further experiments
+    #
+
 
 finish_time = time.time()
 print("time consumption",finish_time-start_time)
